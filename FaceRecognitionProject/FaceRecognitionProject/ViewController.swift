9f89cb1d75585485d7c64b96b5ca97dad6fcb8f1
Özcan BIYIK <ozcanbiyik@outlook.com>
2021-02-17 12:32:15 +0300

    Add files via upload

---
+++ FaceRecognitionProject/FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Özcan BIYIK on 11.07.2020.
+//  Copyright © 2020 Ozcan Biyik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
